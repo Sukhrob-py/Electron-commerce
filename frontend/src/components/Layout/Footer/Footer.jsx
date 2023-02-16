2d41756fb9c8f5e56cd2c1c37c5f4d3a6ac46043
sukhrob-py <sukhrobovlayev@gmail.com>
2023-02-16 05:39:28 -0500

    footer overview

--- frontend/src/components/Layout/Footer/Footer.jsx
+++ frontend/src/components/Layout/Footer/Footer.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from "react";
+
+import { AiOutlineSend } from "react-icons/ai";
+import { TfiHeadphoneAlt } from "react-icons/tfi";
+
+import "./footer.scss";
 
 function Footer() {
   return (
-    <div className='footer'>
+    <div className="footer-wrapper">
       <div className="container">
-        
+        <div className="footer">
+          <div className="footer-form">
+            <div className="footer-form-title">
+              <h2 className="title">Subscribe newsletter</h2>
+            </div>
+            <form>
+              <input type="text" placeholder="Email address" />
+              <button>
+                <AiOutlineSend className="icon" />
+              </button>
+            </form>
+            <div className="footer-contact">
+              <div className="icon-box">
+                <TfiHeadphoneAlt className="icon" />
+              </div>
+              <p className="txt">
+                Call us 24/7 :<br />
+                (+62) 0123 567 789
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
